6e04c1d65503bdf018498489991a9069a800c7ed
xudashuai <xudashuai0819@outlook.com>
2017-03-31 18:11:14 +0800

    1.0

--- Sender.py
+++ Sender.py
@@ -1,68 +1,76 @@
+import getopt
+import os
 import sys
-import getopt,time,threading,os
+import threading
+import time
 
-import Checksum
 import BasicSender
+import Checksum
 
 '''
 This is a skeleton sender class. Create a fantastic transport protocol here.
 '''
+
+
 class Sender(BasicSender.BasicSender):
     def __init__(self, dest, port, filename, debug=False):
         super(Sender, self).__init__(dest, port, filename, debug)
 
     # Main sending loop.
-    msg_size=4000
-    end_size=2**32
-    send_packet={}
-    resend_time=0.5
-    max_now=0
-    def send_msg(self,seqno):
-        if seqno>self.end_size:
+    msg_size = 4000
+    end_size = 2 ** 32
+    send_packet = {}
+    resend_time = 0.5
+    max_now = 0
+
+    def send_msg(self, seqno):
+        if seqno > self.end_size:
             return
-        self.infile.seek(seqno*self.msg_size,os.SEEK_SET)
-        msg=self.infile.read(self.msg_size)
-        msg_type='data'
+        self.infile.seek(seqno * self.msg_size, os.SEEK_SET)
+        msg = self.infile.read(self.msg_size)
+        msg_type = 'data'
         if seqno == 0:
-            msg_type='start'
-        elif msg.__len__()<self.msg_size:
-            msg_type='end'
-            self.end_size=seqno
+            msg_type = 'start'
+        elif msg.__len__() < self.msg_size:
+            msg_type = 'end'
+            self.end_size = seqno
         packet = self.make_packet(msg_type, seqno, msg)
         self.send(packet)
-        self.send_packet[seqno]=(packet,time.time())
+        self.send_packet[seqno] = (packet, time.time())
+
     def start(self):
-        t=threading.Thread(target=self.rec,args=())
+        t = threading.Thread(target=self.rec, args=())
         t.setDaemon(True)
         t.start()
-        self.seqno=0
-        while self.seqno<=self.end_size+1:
-            if self.seqno >self.max_now+5:
+        self.seqno = 0
+        while self.seqno <= self.end_size + 1:
+            if self.seqno > self.max_now + 5:
                 continue
             self.send_msg(self.seqno)
-            self.seqno+=1
+            self.seqno += 1
         try:
             t.join()
         except:
             pass
+
     def rec(self):
         while True:
-            response=self.receive(0.005)
-            if  response is not None :
+            response = self.receive(0.005)
+            if response is not None:
                 if Checksum.validate_checksum(response):
-                    _, seqno, _, _ =self.split_packet(response)
-                    seqno=int(seqno)
-                    if seqno-1==self.end_size:
+                    _, seqno, _, _ = self.split_packet(response)
+                    seqno = int(seqno)
+                    if seqno - 1 == self.end_size:
                         return
                     for x in self.send_packet.items():
-                        if x[0]<seqno:
-                            self.resend_time=(self.resend_time*0.75+(time.time()-x[1][1])*0.5)
+                        if x[0] < seqno:
+                            self.resend_time = (self.resend_time * 0.75 + (time.time() - x[1][1]) * 0.5)
                             self.send_packet.pop(x[0])
-                    self.max_now=max(self.max_now,seqno)
+                    self.max_now = max(self.max_now, seqno)
             for x in self.send_packet.items():
-                if time.time()-x[1][1] > self.resend_time:
+                if time.time() - x[1][1] > self.resend_time:
                     self.send(x[1][0])
-                    self.send_packet[x[0]]=(x[1][0],time.time())
+                    self.send_packet[x[0]] = (x[1][0], time.time())
 
 
 '''
@@ -79,9 +87,10 @@ if __name__ == "__main__":
         print "-d | --debug Print debug messages"
         print "-h | --help Print this usage message"
 
+
     try:
         opts, args = getopt.getopt(sys.argv[1:],
-                               "f:p:a:d", ["file=", "port=", "address=", "debug="])
+                                   "f:p:a:d", ["file=", "port=", "address=", "debug="])
     except:
         usage()
         exit()
@@ -91,7 +100,7 @@ if __name__ == "__main__":
     filename = None
     debug = False
 
-    for o,a in opts:
+    for o, a in opts:
         if o in ("-f", "--file="):
             filename = a
         elif o in ("-p", "--port="):
@@ -101,7 +110,7 @@ if __name__ == "__main__":
         elif o in ("-d", "--debug="):
             debug = True
 
-    s = Sender(dest,port,filename,debug)
+    s = Sender(dest, port, filename, debug)
     try:
         s.start()
     except (KeyboardInterrupt, SystemExit):
